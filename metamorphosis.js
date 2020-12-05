45494f0be612d075823e75e2d95ff855e35d5a89
Kamil Grondys <kamil.grondys@codecool.com>
2020-12-05 15:46:57 +0100

    java-adv

--- metamorphosis.js
+++ metamorphosis.js
@@ -2,7 +2,7 @@ initDragAndDrop();
 
 function initDragAndDrop() {
     shuffleCards();
-
+    initHandlers();
     // Initialize drag & drop elements here
 
 }
@@ -12,4 +12,62 @@ function shuffleCards() {
     for (let i = mixedCardsContainer.children.length; i >= 0; i--) {
         mixedCardsContainer.appendChild(mixedCardsContainer.children[Math.random() * i | 0]);
     }
+}
+
+function initHandlers() {
+    const cards = document.getElementsByClassName("card");
+    const slots = document.getElementsByClassName("card-slot");
+    for(const card of cards){
+        //card.setAttribute('draggable', true)
+        card.addEventListener('dragstart', dragstartHandler);
+        card.addEventListener('dragend', dragendHandler);
+    }
+    for(const slot of slots){
+        slot.addEventListener("dragenter", dragenterHandler);
+        slot.addEventListener("dragover", dragoverHandler);
+        slot.addEventListener("dragleave", dragleavHandler);
+        slot.addEventListener("drop", dropHandler);
+    }
+}
+
+function dragstartHandler(event){
+    toggleCardSlots();
+    event.dataTransfer.setData('text/plain', this.id)
+    console.debug("dragstartHandler called", event.target);
+}
+
+function dragendHandler(event){
+    console.debug("dragendHandler called", event.target);
+    toggleCardSlots(true);
+}
+
+function toggleCardSlots(off = false){
+    const slots = document.getElementsByClassName("card-slot");
+    for( const slot of slots){
+        if(off){
+            slot.classList.remove('card-slot-highlighted');
+        } else {
+            slot.classList.add('card-slot-highlighted');
+        }
+    }
+}
+
+function dragenterHandler(event){
+    console.debug("dragenterHandler called", event.target);
+    event.target.classList.add('card-slot-over');
+    event.preventDefault();
+}
+function dragleavHandler(event){
+    console.debug("dragleavHandler called", event.target);
+    event.target.classList.remove('card-slot-over');
+    event.preventDefault();
+}
+function dragoverHandler(event){
+    event.preventDefault();
+}
+function dropHandler(event){
+    let id = event.dataTransfer.getData('text/plain')
+    event.target.appendChild(document.getElementById(id));
+    console.debug("dropHandler called", id);
+    
 }
