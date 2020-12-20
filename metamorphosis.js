c2c4eba892c96b4d3830fb53b01f971cd490336c
Kamil Grondys <kamil.grondys@codecool.com>
2020-12-20 13:22:35 +0100

    adding card-slots highlighted

--- metamorphosis.js
+++ metamorphosis.js
@@ -16,19 +16,45 @@ function shuffleCards() {
 
 function initHandlers() {
     let cards = document.getElementsByClassName('card');
+    let cardSlots = document.getElementsByClassName('metamorphosis-slots');
     for (const card of cards){
         card.addEventListener("dragstart", dragStartHandler);
         card.addEventListener("dragend", dragEndHandler);
+    }
+    for (const card of cardSlots){
+        card.addEventListener("dragover", dragOverHandler);
+        card.addEventListener("drop", dropHandler);
     }
 }
 
 function dragStartHandler(event){
     console.debug("rozpoczął się drag & drop", event.target);
+    togleCardSlots();
     event.target.classList.add('card-highlighted');
 }
 
 function dragEndHandler(event){
     console.debug("zakończył się drag & drop", event.target);
+    togleCardSlots(true);
     event.target.classList.remove('card-highlighted');
 
+}
+
+function dragOverHandler(event){
+    console.debug("drag over...")
+    event.preventDefault();
+}
+function dropHandler(event){
+    console.debug("drop")
+}
+
+function togleCardSlots(off = false){
+    const slots = document.getElementsByClassName('card-slot');
+    for (const slot of slots){
+        if(off){
+            slot.classList.remove('card-slot-highlighted')
+        } else {
+            slot.classList.add('card-slot-highlighted')
+        }
+    }
 }
